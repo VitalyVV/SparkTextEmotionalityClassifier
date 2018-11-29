7ab859fdd8fadf755e5616c9617e4c82dc7662fd
Alsu <alsu.vakhitova@gmail.com>
2018-11-29 10:14:28 +0300

    Language filter

--- bdproject/src/main/scala/streamDemo.scala
+++ bdproject/src/main/scala/streamDemo.scala
@@ -10,13 +10,14 @@ import org.jsoup.Jsoup
 
 object streamDemo {
   def main(args: Array[String]) {
-    val durationSeconds = 10
+    val durationSeconds = 90
+    //val conf = new SparkConf().setAppName("RSS Spark Application").setIfMissing("spark.master", "local[*]")
     val conf = new SparkConf().setAppName("RSS Spark Application").setIfMissing("spark.master", "local[*]")
     val sc = new SparkContext(conf)
     val ssc = new StreamingContext(sc, Seconds(durationSeconds))
     sc.setLogLevel("ERROR")
 
-    val urls = Seq("https://queryfeed.net/tw?q=%23fantasticbeasts2")
+    val urls = Seq("https://queryfeed.net/tw?token=5bfec0d2-4657-4d2a-98d0-69f3584dc3b3&q=%23weather")
     val stream = new RSSInputDStream(urls, Map[String, String](
       "User-Agent" -> "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
     ), ssc, StorageLevel.MEMORY_ONLY, pollingPeriodInSeconds = durationSeconds)
@@ -24,10 +25,11 @@ object streamDemo {
       val spark = SparkSession.builder().appName(sc.appName).getOrCreate()
       import spark.sqlContext.implicits._
       var df = rdd.toDF()
-      df.show()
+      df.filter($"description".contains("lang=\"en\""))
       val noHTML = udf { s: String => Jsoup.parse(s) }
       df.withColumn("tweet", noHTML(df("description")))
       df.select("tweet")
+      df.show()
     })
 
     // run forever
