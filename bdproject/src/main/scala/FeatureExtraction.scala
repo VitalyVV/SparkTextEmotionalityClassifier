1f93b1c599127f81c66042a71621e9c268e60330
ekaterinaYashkina <twelvedavinci21@gmail.com>
2018-12-01 00:49:47 +0300

    Update FeatureExtraction.scala

--- bdproject/src/main/scala/FeatureExtraction.scala
+++ bdproject/src/main/scala/FeatureExtraction.scala
@@ -1,47 +1,52 @@
-// $example on$
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.feature.{HashingTF, RegexTokenizer, Tokenizer}
-// $example off$
-import org.apache.spark.sql.SQLContext
-import org.apache.spark.{SparkConf, SparkContext}
-
-object FeatureExtraction {
-  def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("FeatureExtraction")
-    val sc = new SparkContext(conf)
-    val sqlContext = new SQLContext(sc)
-
-    //
-    val hdfs_folder = "hdfs://<namenode>/home/user/text_data"
-
-//    val inputDataFrame = sqlContext.createDataFrame(Seq(
-//      (0, "Hi I heard about Spark"),
-//      (1, "I wish Java could use case classes"),
-//      (2, "Logistic,regression,models,are,neat")
-//    )).toDF("label", "tweet")
-
-    val inputDataFrame = loadFileModel(hdfs_folder+"some_file",sc)
-
-    val tokenizer = new Tokenizer().setInputCol("text").setOutputCol("words")
-    val stopWords = new StopWordsRemover().setInputCol(tokenizer.getOutputCol).setOutputCol("cleanWords")
-    val hashingTF = new HashingTF()
-      .setNumFeatures(1000)
-      .setInputCol(stopWords.getOutputCol)
-      .setOutputCol("features")
-
-    val model = new LogisticRegression() //another model should be used
-    val pipeline = new Pipeline()
-      .setStages(Array(tokenizer, stopWords, hashingTF, model))
+import org.apache.spark.ml.feature.{HashingTF, RegexTokenizer, StopWordsRemover}
+import org.apache.spark.sql.{Column, DataFrame}
+import org.apache.spark.sql.functions.{monotonically_increasing_id, regexp_replace, col}
 
 
-    //TODO model
-// maybe its worth adding idf too - can see after getting training
-    sc.stop()
+class FeatureExtraction {
+
+  def clean()(col: Column): Column = {
+
+    var reg1 = regexp_replace(col, "\\' ", " ")
+    reg1 = regexp_replace(reg1, "\\'\\w+", "")
+    reg1 = regexp_replace(reg1, "e+", "e")
+    reg1 = regexp_replace(reg1, "o+", "o")
+    reg1 = regexp_replace(reg1, "a+", "a")
+    reg1 = regexp_replace(reg1, "i+", "i")
+    reg1 = regexp_replace(reg1, "y+", "y")
+    reg1 = regexp_replace(reg1, "(?<!\\s)in", "ing")
+    reg1
   }
 
-  def loadFileModel(path:String, sc:SparkContext) = {
+  def normalize(datafr: DataFrame, column: String, idCol: String): DataFrame = {
+    var norFrame = datafr.select(clean()(col(column)).as("normalized"))
+    norFrame.show()
 
-    //returns DataFrame from txt file in format label;text;class
+    norFrame = norFrame.withColumn("id", monotonically_increasing_id())
+
+    norFrame
+
+    //      val mydf = datafr.as("df1").join(norFrame.as("df2"), datafr(idCol) === norFrame("id"), "inner")
+    //        .select("df1."+idCol,  "df2.normalized")
+    //      mydf.withColumnRenamed("normalized", column)
+
+  }
+
+
+  def buildFeatureSelection(inputColumn: String): (RegexTokenizer, StopWordsRemover, HashingTF) = {
+    val regexTokenizer = new RegexTokenizer()
+      .setInputCol(inputColumn)
+      .setOutputCol("words")
+      .setPattern("(\\@\\w+)|[^\\W+']|(http\\:\\/\\/\\w+)|(https\\:\\/\\/\\w+)")
+
+    val stopWords = new StopWordsRemover().
+      setInputCol(regexTokenizer.getOutputCol).setOutputCol("cleanWords")
+
+    val hashingTF = new HashingTF()
+      .setNumFeatures(1000)
+      .setInputCol(regexTokenizer.getOutputCol)
+      .setOutputCol("features")
+
+    (regexTokenizer, stopWords, hashingTF)
   }
 }
