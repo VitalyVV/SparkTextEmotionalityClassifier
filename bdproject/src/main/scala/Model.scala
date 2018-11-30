f762632df2c43951a8771ed98c3ba4ceb8067ce5
ekaterinaYashkina <twelvedavinci21@gmail.com>
2018-12-01 01:00:57 +0300

    Update Model.scala

--- bdproject/src/main/scala/Model.scala
+++ bdproject/src/main/scala/Model.scala
@@ -31,9 +31,17 @@ object Model {
 
     val Array(train, test) = df.randomSplit(Array[Double](0.7, 0.3))
 
-    val dfs = normalize(train.toDF("ItemID", "label", "SentimentText"), "SentimentText", 
-      "ItemID", "label")
+    val extractor = new FeatureExtraction()
+//     val dfs = normalize(train.toDF("ItemID", "label", "SentimentText"), "SentimentText", 
+//       "ItemID", "label")
 
+    val trainData = train.toDF("ItemID", "label", "SentimentText")
+    var dfs = extractor.normalize(trainData, "SentimentText",  "ItemID")
+    
+    dfs = trainData.as("df1").join(dfs.as("df2"), trainData("ItemID") === dfs ("id"), "inner")
+      .select("df1."+"ItemID", "df1."+"label", "df2.normalized")
+    dfs = dfs.withColumnRenamed("normalized", "SentimentText")
+    
     val lsvc = new LinearSVC()
       .setMaxIter(10)
       .setRegParam(0.1)
@@ -52,47 +60,47 @@ object Model {
     session.stop()
   }
   
-  def clean()(col:Column): Column = {
+//   def clean()(col:Column): Column = {
 
-    var reg1 = regexp_replace(col,"\\' ", " ")
-    reg1 = regexp_replace(reg1,"\\'\\w+", "")
-    reg1 = regexp_replace(reg1, "e+", "e")
-    reg1 = regexp_replace(reg1, "o+", "o")
-    reg1 = regexp_replace(reg1, "a+", "a")
-    reg1 = regexp_replace(reg1, "i+", "i")
-    reg1 = regexp_replace(reg1, "y+", "y")
-    reg1 = regexp_replace(reg1, "(?<!\\s)in", "ing")
-    reg1
-  }
+//     var reg1 = regexp_replace(col,"\\' ", " ")
+//     reg1 = regexp_replace(reg1,"\\'\\w+", "")
+//     reg1 = regexp_replace(reg1, "e+", "e")
+//     reg1 = regexp_replace(reg1, "o+", "o")
+//     reg1 = regexp_replace(reg1, "a+", "a")
+//     reg1 = regexp_replace(reg1, "i+", "i")
+//     reg1 = regexp_replace(reg1, "y+", "y")
+//     reg1 = regexp_replace(reg1, "(?<!\\s)in", "ing")
+//     reg1
+//   }
 
-  def buildFeatureSelection(inputColumn: String): (RegexTokenizer, StopWordsRemover, HashingTF) = {
-    val regexTokenizer = new RegexTokenizer()
-      .setInputCol(inputColumn)
-      .setOutputCol("words")
-      .setPattern("(\\@\\w+)|\\W+|(http\\:\\/\\/\\w+)|(https\\:\\/\\/\\w+)")
+//   def buildFeatureSelection(inputColumn: String): (RegexTokenizer, StopWordsRemover, HashingTF) = {
+//     val regexTokenizer = new RegexTokenizer()
+//       .setInputCol(inputColumn)
+//       .setOutputCol("words")
+//       .setPattern("(\\@\\w+)|\\W+|(http\\:\\/\\/\\w+)|(https\\:\\/\\/\\w+)")
 
-    val stopWords = new StopWordsRemover().
-      setInputCol(regexTokenizer.getOutputCol).setOutputCol("cleanWords")
+//     val stopWords = new StopWordsRemover().
+//       setInputCol(regexTokenizer.getOutputCol).setOutputCol("cleanWords")
 
-    val hashingTF = new HashingTF()
-      .setNumFeatures(1000)
-      .setInputCol(regexTokenizer.getOutputCol)
-      .setOutputCol("features")
+//     val hashingTF = new HashingTF()
+//       .setNumFeatures(1000)
+//       .setInputCol(regexTokenizer.getOutputCol)
+//       .setOutputCol("features")
 
 
-    (regexTokenizer, stopWords, hashingTF)
-  }
+//     (regexTokenizer, stopWords, hashingTF)
+//   }
   
-  def normalize(datafr: DataFrame, column: String, idCol: String, labelCol:String): DataFrame = {
-    var norFrame = datafr.select(clean()(col(column)).as("normalized"))
-    norFrame.show()
+//   def normalize(datafr: DataFrame, column: String, idCol: String, labelCol:String): DataFrame = {
+//     var norFrame = datafr.select(clean()(col(column)).as("normalized"))
+//     norFrame.show()
 
-    norFrame = norFrame.withColumn("id", monotonically_increasing_id())
+//     norFrame = norFrame.withColumn("id", monotonically_increasing_id())
 
-    val mydf = datafr.as("df1").join(norFrame.as("df2"), datafr(idCol) === norFrame("id"), "inner")
-      .select("df1."+idCol, "df1."+labelCol, "df2.normalized")
+//     val mydf = datafr.as("df1").join(norFrame.as("df2"), datafr(idCol) === norFrame("id"), "inner")
+//       .select("df1."+idCol, "df1."+labelCol, "df2.normalized")
 
-    mydf.withColumnRenamed("normalized", column)
+//     mydf.withColumnRenamed("normalized", column)
 
-  }
+//   }
 }
