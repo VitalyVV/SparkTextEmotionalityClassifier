c517dd845c9b7c5abf249e3fa8fddb5c3a02a1c3
ekaterinaYashkina <twelvedavinci21@gmail.com>
2018-12-01 12:59:30 +0300

    Update streamDemo.scala

--- bdproject/src/main/scala/streamDemo.scala
+++ bdproject/src/main/scala/streamDemo.scala
@@ -17,6 +17,8 @@ object streamDemo {
     val ssc = new StreamingContext(sc, Seconds(durationSeconds))
     sc.setLogLevel("ERROR")
 
+    extractor = new FeatureExtraction()
+    
     val urls = Seq("https://queryfeed.net/tw?token=5bfec0d2-4657-4d2a-98d0-69f3584dc3b3&q=%23weather")
     val stream = new RSSInputDStream(urls, Map[String, String](
       "User-Agent" -> "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
